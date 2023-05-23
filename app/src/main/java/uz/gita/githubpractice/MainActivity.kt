a5ce2ba380af9c501b189f00e9b6d713878a3ba7
Olmos <olmosikromovich19jasontodd@gmail.com>
2023-05-23 11:11:24 +0500

    Compose Starter text

--- app/src/main/java/uz/gita/githubpractice/MainActivity.kt
+++ app/src/main/java/uz/gita/githubpractice/MainActivity.kt
@@ -19,7 +19,7 @@ class MainActivity : ComponentActivity() {
             GitHubPracticeTheme {
                 // A surface container using the 'background' color from the theme
                 Surface(modifier = Modifier.fillMaxSize(), color = MaterialTheme.colorScheme.background) {
-                    Greeting("Android")
+                    Text(text = "Compose Starter!")
                 }
             }
         }
