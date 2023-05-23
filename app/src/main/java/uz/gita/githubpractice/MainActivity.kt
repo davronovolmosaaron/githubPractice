780e559a36ad3c0d665d7cb434646ba068e31c54
Olmos <olmosikromovich19jasontodd@gmail.com>
2023-05-23 14:04:56 +0500

    Sample

--- app/src/main/java/uz/gita/githubpractice/MainActivity.kt
+++ app/src/main/java/uz/gita/githubpractice/MainActivity.kt
@@ -1,15 +1,37 @@
 package uz.gita.githubpractice
 
+import android.content.res.Configuration
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.border
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.layout.wrapContentHeight
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontStyle
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.tooling.preview.Devices
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.TextUnit
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import uz.gita.githubpractice.ui.theme.GitHubPracticeTheme
 
 class MainActivity : ComponentActivity() {
@@ -19,25 +41,58 @@ class MainActivity : ComponentActivity() {
             GitHubPracticeTheme {
                 // A surface container using the 'background' color from the theme
                 Surface(modifier = Modifier.fillMaxSize(), color = MaterialTheme.colorScheme.background) {
-                    Text(text = "Compose Starter!")
+
                 }
             }
         }
     }
 }
 
+data class Message(val author: String, val body: String)
+
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
+fun MessageCard(msg: Message) {
+    Row(modifier = Modifier.padding(8.dp), verticalAlignment = Alignment.CenterVertically) {
+        Image(
+            modifier = Modifier
+                .size(64.dp)
+                .clip(CircleShape)
+                .border(width = 2.dp, color = MaterialTheme.colorScheme.primary, shape = CircleShape),
+            painter = painterResource(id = R.drawable.ic_launcher_foreground),
+            contentDescription = "Default Image"
+        )
+
+        Spacer(modifier = Modifier.width(8.dp))
+
+        Column() {
+            Text(
+                text = "Laugh! ${msg.author}",
+                style = MaterialTheme.typography.titleSmall,
+                color = MaterialTheme.colorScheme.secondary
+            )
+            Spacer(modifier = Modifier.height(4.dp))
+            Text(
+                text = "Question: ${msg.body}",
+                style = MaterialTheme.typography.bodySmall,
+                color = MaterialTheme.colorScheme.onBackground
+            )
+        }
+    }
 }
 
-@Preview(showBackground = true)
+@Preview(
+    showBackground = true,
+    name = "MessageCard",
+    group = "group 1",
+    device = Devices.PIXEL_4,
+    showSystemUi = true,
+    uiMode = Configuration.UI_MODE_NIGHT_YES
+)
 @Composable
-fun GreetingPreview() {
-    GitHubPracticeTheme {
-        Greeting("Android")
+fun PreviewMessageCard() {
+    GitHubPracticeTheme() {
+        Surface() {
+            MessageCard(msg = Message("Olmos", "What we call someone without body and nose?\nAnswer: No body nose😂 A!,A!,A!"))
+        }
     }
 }
